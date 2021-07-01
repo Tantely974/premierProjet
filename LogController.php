1364ba89819586e9901903b08d4b063a254f6c4a
Tantely974 <81219652+Tantely974@users.noreply.github.com>
2021-07-01 15:31:57 +0200

    Add files via upload

---
+++ LogController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Aecf\Esmile\Gui\AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @Route("/api/log")
+ */
+class LogController extends Controller
+{
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     *
+     * @Route("", name="log", methods={"POST"})
+     */
+    public function logAction(Request $request)
+    {
+        $this->get('logger')->error(sprintf(
+            '[UI TECHNICAL ERROR] a technical error%s%s occured when calling %s (%s) with data %s.',
+            $request->get('status') ? ' (response status: '.$request->get('status').')' : '',
+            $request->get('responseText') ? ' (response text: '.$request->get('responseText').')' : '',
+            $request->get('url'),
+            $request->get('method'),
+            $request->get('data')
+        ));
+
+        return new Response('', Response::HTTP_NO_CONTENT);
+    }
+}
